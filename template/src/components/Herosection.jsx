d1b188076d1c87fdb00996c3f8d1d3a166ab4fee
Nandisa <shangenandisa@gmail.com>
2025-09-30 10:49:59 +0200

    Edited Herosection

--- template/src/components/Herosection.jsx
+++ template/src/components/Herosection.jsx
@@ -6,7 +6,7 @@ export default function Herosection() {
         <img src="./src/assets/cover-2.webp" alt="cover-image" className="cover-image"/>
         <div className="herosection-overlay"></div>
         <div className="herosection-text">
-          <h2 className="herosection-heading">Louis Vuitton</h2>
+          <h2 className="herosection-heading">Louis Vuitton x Virgil Abloh</h2>
           <p className="herosection-para">Discover the new Spring-Summer 2024 collection,<br/> where creativity meets elegance in every stitch.</p>
           <button className="explore-button">Explore the collection</button>
         </div>
